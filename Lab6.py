0fb712861925eaf918bfd7a939b0e1f3e97e1306
Dionming1 <116833769+Dionming1@users.noreply.github.com>
2023-03-09 14:42:45 -0500

    Encoded Dion

---
+++ Lab6.py
@@ -0,0 +1,28 @@
+#Dion Ming
+
+def menu():
+    print('1. Encoder')
+    print('2. Decoder')
+    print('3. Exit')
+def encode(pw):
+    newpw = ''
+    for i in pw:
+        num = int(i)+3
+        newpw += str(num)
+    #print(newpw)
+    return newpw
+
+def main():
+
+    while True:
+        menu()
+        menuop = input('Select an option: ')
+
+        if menuop == 1:
+            passwd = input('Input a password to be encoded: ')
+            encpass = encode(passwd)
+        if menuop == 2:
+
+        if menuop == 3:
+            break
+
